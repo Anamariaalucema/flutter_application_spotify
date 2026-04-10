3c0c320ae9e41cc6981cecf7b024999d6ce14c11
Anamariaalucema <aalucema374@soyudemedellin.edu.co>
2026-04-10 17:44:15 -0500

    archivo cancion

---
+++ lib/models/cancion.dart
@@ -0,0 +1,17 @@
+class Cancion {
+  final int id;
+  final String nombre; 
+  final String artista;
+  final String album;
+  final String imagen;
+  final String archivo; 
+
+  Cancion({
+    required this.id,
+    required this.nombre,
+    required this.artista,
+    required this.album,
+    required this.imagen,
+    required this.archivo,
+  });
+}
