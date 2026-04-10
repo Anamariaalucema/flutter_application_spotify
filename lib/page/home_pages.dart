165dbd16727355f945722af38ee669c8eb47a094
Anamariaalucema <aalucema374@soyudemedellin.edu.co>
2026-04-10 17:32:04 -0500

    carpetas y archivo iniciales

---
+++ lib/page/home_pages.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePages extends StatefulWidget {
+
+  const HomePages({super.key});// Constructor de la clase HomePages
+
+  @override
+  State<HomePages> createState() => _HomePagesState();
+
+}
+
+class _HomePagesState extends State<HomePages> {
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
