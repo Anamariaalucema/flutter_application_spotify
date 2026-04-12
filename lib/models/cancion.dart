e7351a8c52fb82444e3ebac74d45feeb5d956fe0
Tatiana Murillo <tatianamurillomosquera1@gmail.com>
2026-04-12 14:49:31 -0500

    Model module created

--- lib/models/cancion.dart
+++ lib/models/cancion.dart
@@ -12,4 +12,25 @@ class Cancion {
     required this.album,
     required this.imagen,
   });
+
+  factory Cancion.fromJson(Map<String, dynamic> json) {
+    return Cancion(
+      id: json['id'] as int,
+      nombre: (json['nombre'] ?? '') as String,
+      artista: (json['artista'] ?? '') as String,
+      album: (json['album'] ?? '') as String,
+      imagen: (json['imagen'] ?? '') as String,
+    );
+  }
+
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'nombre': nombre,
+      'artista': artista,
+      'album': album,
+      'imagen': imagen,
+    };
+  }
 }
